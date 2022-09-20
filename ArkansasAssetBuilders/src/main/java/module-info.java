3f6257c5dab486e4bc02fbaaf87373ac29fa1d5d
Andrei Stoyanow <46803116+astoyanow@users.noreply.github.com>
2022-09-20 11:56:23 -0500

    Init JAR file
    First build, working JAR file created with manifest file. Warning throws for unnamed module but should be okay.

--- ArkansasAssetBuilders/src/main/java/module-info.java
+++ ArkansasAssetBuilders/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.aab.arkansasassetbuilders {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.aab.arkansasassetbuilders to javafx.fxml;
