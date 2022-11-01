d211d23e1f214350f6b29270a4aa46f5a90c5a80
Matthew Kalahiki <kalahikimw@hendrix.edu>
2022-11-01 14:20:41 -0500

    hopefully this fixes merge errors

--- ArkansasAssetBuilders/src/main/java/com/aab/arkansasassetbuilders/searchAndFilterLauncher.java
+++ ArkansasAssetBuilders/src/main/java/com/aab/arkansasassetbuilders/searchAndFilterLauncher.java
@@ -2,24 +2,23 @@ package com.aab.arkansasassetbuilders;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.sql.*;
 
-import java.io.IOException;
+import java.util.Objects;
 
 public class searchAndFilterLauncher extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(searchAndFilterControler.class.getResource("Search-and-filter-screen.fxml"));
-        //Parent root = FXMLLoader.load(getClass().gerResource("Search-and-filter-screen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        //stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("UploadScreen.fxml")));
+        Scene scene = new Scene(root, 300, 275);
+        stage.setScene(scene);
+        stage.show();
     }
 }
